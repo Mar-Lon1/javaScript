38c49cb2c931e8afb2905b85330f2dcc6d8b1907
Marlon Armah <marlonarmah@gmail.com>
2026-05-09 15:57:24 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+// strings
+let a = "name"
+
+let b = "password"
+
+let c = "email"
+
+let d = "address"
+
+let e = "phone"
+
+// numbers
+let f = 10
+
+let g = 11
+
+let h = 24
+
+let i = 30
+
+let j = 343
+
+
+//boolean
+
+let hasEmail = true
+
+let hasPhone = true
+
+let hasAddress = false
+
+let hasPassword = true
+
+let hasName = false
+
+
+//operators
+
+console.log(a + b)
+
+console.log(b === f)
+
+console.log(b && c)
+
+console.log(f || j)
+
+console.log(c !== f)
